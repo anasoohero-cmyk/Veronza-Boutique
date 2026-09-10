4234762d6714c075f2b7698c3d508b40ce09744c
anasoohero-cmyk <anasoohero@gmail.com>
2026-09-10 22:48:19 +0200

    Add deployment version endpoint for silent app updates

---
+++ api/version.js
@@ -0,0 +1,6 @@
+module.exports = (req, res) => {
+  res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
+  res.setHeader('Pragma', 'no-cache');
+  res.setHeader('Expires', '0');
+  res.status(200).json({ version: process.env.VERCEL_GIT_COMMIT_SHA || process.env.VERCEL_DEPLOYMENT_ID || 'development' });
+};
